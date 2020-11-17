1df6d770a47a70fd9ae9cb18e027379e2d83b65a
sadeceben <cemaltiryaki01@gmail.com>
2020-11-18 02:41:50 +0300

    Çrş 18 Kas 2020 02:41:41 +03

--- sub_3num.go
+++ sub_3num.go
@@ -11,25 +11,49 @@ import (
 	"strings"
 )
 
-func control(err error) {
+// log file path
+const log_f_path string = "logs/logs.txt"
+
+// GoogleEnum Variable
+const ggl_name string = "Google"
+const google_url string = "https://google.com/search?q={query}&btnG=Search&hl=en-US&biw=&bih=&gbv=1&start={page_no}&filter=0"
+const google_re string = "https://www.google.com/recaptcha/api.js"
+
+// PassiveDNS Variable
+const passive_url string = "http://ptrarchive.com/tools/search.htm?label="
+const passive_name string = "PassiveDNS"
+
+type SearchEngine interface {
+	GoogleEnum() // [+]
+	PassiveDNS() // [+]
+	control()    // [+]
+	loger()      // [+]
+	parser()     // [-]
+}
+
+type Engine struct {
+}
+
+func (e Engine) control(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func loger(prefix, description string) {
-	file, err := os.OpenFile("logs/logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	control(err)
+func (e Engine) loger(prefix, description string) {
+	file, err := os.OpenFile(log_f_path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	e.control(err)
 	defer file.Close()
 	logger := log.New(file, prefix, log.LstdFlags)
 	logger.Println(description)
 }
 
-func parser(body, domain, search_engine string) (is_it bool) {
+func (e Engine) parser(body, domain, search_engine string) (is_it bool) {
 	var reg_x string
-	if strings.Contains(search_engine, "Google") {
+	e.loger("RUNNING : ", "Parser function running")
+	if strings.Contains(search_engine, ggl_name) {
 		reg_x = "(http|ftp|https)://([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?"
-	} else if strings.Contains(search_engine, "PassiveDNS") {
+	} else if strings.Contains(search_engine, passive_name) {
 		reg_x = "<td>(.*?)</td>"
 	}
 
@@ -38,7 +62,7 @@ func parser(body, domain, search_engine string) (is_it bool) {
 	list := re.FindAllString(body, -1)
 	for _, links := range list {
 		if strings.Contains(links, domain) {
-			if strings.Contains(search_engine, "PassiveDNS") {
+			if strings.Contains(search_engine, passive_name) {
 				links = strings.Replace(links, "<td>", "", -1)
 				links = strings.Replace(links, " [TR]</td>", "", -1)
 				fmt.Println("|----> : " + "http://" + links + " OR " + "https://" + links)
@@ -49,66 +73,67 @@ func parser(body, domain, search_engine string) (is_it bool) {
 			}
 		}
 	}
+	e.loger("SUCCESFLY : ", "Parser function finished")
 	return
 }
 
-func GoogleEnum(query string) {
-	base_url := "https://google.com/search?q={query}&btnG=Search&hl=en-US&biw=&bih=&gbv=1&start={page_no}&filter=0"
+func (e Engine) GoogleEnum(query string) {
+	base_url := google_url
 	base_url = strings.Replace(base_url, "{query}", "site:*.*."+query, -1)
 	new_url := ""
 
-	loger("RUNNING : ", "GoogleEnum function is runnig")
+	e.loger("RUNNING : ", "GoogleEnum function is runnig")
 
 	for i := 0; i < 100; i++ {
 		new_url = strings.Replace(base_url, "{page_no}", strconv.Itoa(i*10), -1)
 		resp, err := http.Get(new_url)
-		control(err)
+		e.control(err)
 		if resp.Status == "404 Not Found" {
-			loger("ERROR : ", "GoogleEnum function get request status is : "+resp.Status)
+			e.loger("ERROR : ", "GoogleEnum function get request status is : "+resp.Status)
 			break
 		}
 		defer resp.Body.Close()
 
 		body, err := ioutil.ReadAll(resp.Body)
-		control(err)
+		e.control(err)
 
-		if parser(string(body), query, "Google") {
+		if e.parser(string(body), query, ggl_name) {
 			fmt.Println(i)
-		} else if strings.Contains(string(body), "https://www.google.com/recaptcha/api.js") {
-			loger("ERROR : ", "GoogleEnum funciton is caught google recaptcha")
+		} else if strings.Contains(string(body), google_re) {
+			e.loger("ERROR : ", "GoogleEnum funciton is caught google recaptcha")
 			break
 		} else {
-			loger("ERROR : ", "Last Page")
+			e.loger("ERROR : ", "Last Page")
 			break
 		}
 	}
-	loger("STOP : ", "GoogleEnum function is stop")
+	e.loger("STOP : ", "GoogleEnum function is stop")
 }
 
-func PassiveDNS(domain string) {
-	base_url := "http://ptrarchive.com/tools/search.htm?label=" + domain
-	loger("RUNNING : ", "PassiveDNS running")
+func (e Engine) PassiveDNS(domain string) {
+	base_url := passive_url + domain
+	e.loger("RUNNING : ", "PassiveDNS running")
 
 	resp, err := http.Get(base_url)
-	control(err)
+	e.control(err)
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-	control(err)
-	if parser(string(body), domain, "PassiveDNS") {
-		loger("SUCCESFLY : ", "PassiveDNS have finished")
+	e.control(err)
+	if e.parser(string(body), domain, passive_name) {
+		e.loger("SUCCESFLY : ", "PassiveDNS have finished")
 	} else {
-		loger("ERROR : ", "cannot runnig PassiveDNS")
+		e.loger("ERROR : ", "cannot runnig PassiveDNS")
 	}
 
 }
 
 func main() {
 	var domain string
+	engine := Engine{}
 	fmt.Print("Enter your Domain : ")
 	fmt.Scan(&domain)
 
-	//	GoogleEnum("firat.edu.tr")
-	PassiveDNS(domain)
+	engine.PassiveDNS(domain)
 }
